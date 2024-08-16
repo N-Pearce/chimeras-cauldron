0b896cdd21b21d73d7a0c4e80df11f9950e2148e
skyla <nathanchrispearce@gmail.com>
2024-08-15 19:48:18 -0600

    removed unnecessary headers in api calls

--- src/api-5e/api.js
+++ src/api-5e/api.js
@@ -24,13 +24,12 @@ class Dnd5eApi {
     //there are multiple ways to pass an authorization token, this is how you pass it in the header.
     //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
     const url = `${BASE_URL}/${endpoint}`;
-    const headers = { Authorization: `Bearer dsvyjghbnk` };
     const params = (method === "get")
         ? data
         : {};
 
     try {
-      return (await axios({ url, method, data, params, headers })).data;
+      return (await axios({ url, method, data, params })).data;
     } catch (err) {
       console.error("API Error:", err.response);
       let message = err.response.data.error.message;
